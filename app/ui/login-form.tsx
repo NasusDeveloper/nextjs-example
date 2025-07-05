e586f0c6d3a72dfb11647e645d60c7d358e23319
Nasus Reformed <nasus.informatic.engineering@gmail.com>
2025-07-05 03:05:37 -0400

    Add metadata. Add Open Graph image using metadata. Adda favicon using metadata

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -11,7 +11,6 @@ import { Button } from '@/app/ui/button';
 import { useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
 import { useSearchParams } from 'next/navigation';
- 
 export default function LoginForm() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
